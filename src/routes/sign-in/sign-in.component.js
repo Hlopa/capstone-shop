b2a5341feea637082edaaced7da7c30de83c4d03
Olga Barishevskaya <bariolga@yandex.ru>
2022-09-06 01:04:15 +0300

    Authentication + Firebase

--- src/routes/sign-in/sign-in.component.js
+++
@@ -1,9 +0,0 @@
-const SignIn = () => {
-  return(
-    <div>
-      <h1>Sign in bb</h1>
-    </div>
-  )
-}
-
-export default SignIn
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,71 @@
+import { initializeApp } from "firebase/app";
+import {getAuth, signInWithPopup, GoogleAuthProvider,
+createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth'
+
+import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAOfAqy1i8OC7BTp17my5Kfd_EefnzqcOo",
+  authDomain: "capstone-shop-1bebe.firebaseapp.com",
+  projectId: "capstone-shop-1bebe",
+  storageBucket: "capstone-shop-1bebe.appspot.com",
+  messagingSenderId: "260894373838",
+  appId: "1:260894373838:web:b726b329c1828668849d9f"
+};
+
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.getCustomParameters({
+  prompt: 'select_account'
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+
+//Инициализируем базу данных
+export const db = getFirestore();
+
+//Берет данные с аутентификации и заносит в бд
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
+  if(!userAuth) return;
+  //принимает 3 параметра - 1)база данных куда записывать 2)название коллекции 3)уникальный идентификатор каждого элемента коллекции (при логине firebase каждому присылает уникальный id)
+  const  userDocRef = doc(db, 'users', userAuth.uid);
+  const userSnaphot = await getDoc(userDocRef)
+
+   //if user data not exist - create /set the document
+  if(!userSnaphot.exists()){
+    const {displayName, email} = userAuth;
+    const createAt = new Date();
+
+    try{
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createAt,
+        ...additionalInformation
+      })
+    } catch (error) {
+        console.log("error creating the user", error.message);
+    }
+  }
+
+  return userDocRef
+
+  //if user data exist - do nothing
+}
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if(!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
+}
+
+
+export const signInUserWithEmailAndPassword = async (email, password) => {
+  if(!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
+}
