abbee69d486fdbaf2106408074da59e2f8737546
Olga Barishevskaya <bariolga@yandex.ru>
2023-01-09 21:12:02 +0300

    redux extended tools

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,28 @@
 import { applyMiddleware, compose, createStore  } from "redux";
 import {logger} from 'redux-logger';
 import { rootReducer } from "./root-reducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const middleWares = [logger];
-const composedEnhancers = compose(applyMiddleware(...middleWares))
+const middleWares = [process.env.NODE_ENV !== 'productiont' && logger].filter(Boolean);
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+
+//Если используем Redux DevTools в хроме
+const composedEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares))
+
+//если не ипользуем Redux DevTools
+// const composedEnhancers = compose(applyMiddleware(...middleWares))
+
+const persisConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persisConfig, rootReducer);
+
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
